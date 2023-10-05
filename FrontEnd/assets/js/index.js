e418b5e639a3e30a07c6d17a3b2fd44923f48902
sebtech-run <sebtech.run@outlook.fr>
2023-10-05 13:52:59 +0400

    Ajout button + page login

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,9 +1,11 @@
 //Appel fetch de mon API
 
+const works = []
 fetch('http://localhost:5678/api/works')
 .then((response) => response.json())
 .then((data) => {
     data.forEach((work) => {
+        works.push(work)
         const figure = createAllBalise(work)
         imagesContainer.appendChild(figure)
     })
@@ -26,4 +28,29 @@ function createAllBalise(work) {
 
     return figure
 }
+
+
+
+fetch('http://localhost:5678/api/categories')
+.then((response) => response.json())
+.then((data) => {
+    data.forEach((categorie) => {
+        const button = document.createElement('button')
+        button.innerText = categorie.name
+        button.classList.add('button-filter')
+        button.addEventListener("click", function() {
+            imagesContainer.innerHTML=""
+            works.forEach(work => {
+                if(work.category.id === categorie.id) {
+                    const figure = createAllBalise(work)
+                    imagesContainer.appendChild(figure)
+                }
+            })
+        })
+        document.querySelector('.filters').appendChild(button)
+    })
+}
+)
+
+
 
