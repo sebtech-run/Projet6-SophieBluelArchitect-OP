cfcb60327c5f667ab7f31db934ab57898aa89838
sebtech-run <sebtech.run@outlook.fr>
2023-10-05 07:35:22 +0400

    Function pour création gallery

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,14 +1,17 @@
+//Appel fetch de mon API
+
 fetch('http://localhost:5678/api/works')
 .then((response) => response.json())
 .then((data) => {
     data.forEach((work) => {
-        
         const figure = createAllBalise(work)
         imagesContainer.appendChild(figure)
     })
 })
 
 const imagesContainer = document.querySelector('.gallery')
+
+//Function qui crée toutes les balises et cible des valeurs pour le DOM
 
 function createAllBalise(work) {
     const figure = document.createElement('figure')
